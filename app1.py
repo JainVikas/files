a910f1e58c79a0de73834b17bc4409a96b47ede2
Jain <vikas.e.jain@accenture.com>
2017-05-11 16:38:16 +0530

    new app

--- app1.py
+++ app1.py
@@ -38,7 +38,7 @@ def webhook():
 def processRequest(req):
     res = 0
     if req.get("result").get("action") == "help.emotion.info":
-        if score<0.2
+        if score < 0.2:
             res = makeEmotionSadWebhookResult()
         else
             res = makeEmotionHappyWebhookResult()
