d8e52014f4ae5ad0163037cb0e1c1418cdd73bbe
Jain <vikas.e.jain@accenture.com>
2017-05-12 09:26:01 +0530

    test 1

--- app.py
+++ app.py
@@ -50,12 +50,9 @@ def processRequest(req):
     else:
         from urllib2 import Request, urlopen, URLError
         request = Request('http://placekitten.com/')
-        try:
-                response = urlopen(request)
-                kittens = response.read()
-                res = kittens[559:1000]
-        except URLError, e:
-            print 'No kittez. Got an error code:', e
+        response = urlopen(request)
+        kittens = response.read()
+        res = kittens[559:1000]  
     return {
         "speech": res,
         "displayText": res,
